c3d79e020189fc92134c94e05429aa9b7adec13a
Mikhail <mixa.penkin@gmail.com>
2024-02-26 16:59:17 +0300

    lr1 done

--- agent.js
+++ agent.js
@@ -2,8 +2,10 @@ const Msg = require('./msg')
 const readline = require('readline')
 const Flags = require('./flags')
 
+const round = a => Math.round(a * 100) / 100
+
 class Agent {
-	constructor() {
+	constructor(team, strat = null) {
 		this.position = 'l' // По умолчанию ~ левая половина поля
 		this.run = false // ИГра начата
 		this.act = null // Действия
@@ -12,6 +14,9 @@ class Agent {
 			input: process.stdin,
 			output: process.stdout,
 		})
+		this.team = team
+		this.strat = strat
+
 		this.rl.on('line', input => {
 			// Обработка строки из кон—
 			if (this.run) {
@@ -54,49 +59,110 @@ class Agent {
 	}
 	analyzeEnv(msg, cmd, p) {
 		// Анализ сообщения
-		if (cmd == 'see') {
-			let observedFlags = []
+		if (cmd === 'see') {
+			if (this.strat && this.strat.name === 'spin' && p[0] > 100) {
+				this.act = { n: 'turn', v: this.strat.speed }
+			}
+			console.log('---------------------------------------------------')
+
+			const observedFlags = []
+			const opponents = []
 
 			const xs = []
 			for (let i = 1; i < p.length; i++) {
 				let flagName = p[i].cmd.p.join('')
 				if (Object.keys(Flags).includes(flagName) && p[i].p.length >= 2) {
+					// Flag
 					let x = Flags[flagName].x
 					if (!xs.includes(x)) {
 						observedFlags.push(p[i])
 						xs.push(x)
 					}
+				} else if (p[i].cmd.p[0] === 'p' && p[i].cmd.p[1] !== this.team) {
+					// Opponent
+					opponents.push(p[i])
 				}
 			}
 
 			if (observedFlags.length < 3) {
-				console.log('Вижу меньше 3 флагов')
+				console.log('В зоне видимости нет трех флагов')
 				return
 			}
+
 			observedFlags.sort((a, b) => a.p[0] - b.p[0])
-			let extractFlagCoordsAndDistance = observedFlag => {
+
+			const extractFlagCoordsAndDistance = observedFlag => {
 				let flagName = observedFlag.cmd.p.join('')
-				return [Flags[flagName].x, Flags[flagName].y, observedFlag.p[0]] // X, Y, расстояние до флага
+				return [
+					Flags[flagName].x,
+					Flags[flagName].y,
+					observedFlag.p[0],
+					observedFlag.p[1],
+				]
 			}
-			let [x1, y1, d1] = extractFlagCoordsAndDistance(observedFlags[0])
-			let [x2, y2, d2] = extractFlagCoordsAndDistance(observedFlags[1])
-			let [x3, y3, d3] = extractFlagCoordsAndDistance(observedFlags[2])
-			let alpha1 = (y1 - y2) / (x2 - x1)
-			let beta1 =
-				(y2 * y2 - y1 * y1 + x2 * x2 - x1 * x1 + d1 * d1 - d2 * d2) /
-				(2 * (x2 - x1))
-			let alpha2 = (y1 - y3) / (x3 - x1)
-			let beta2 =
-				(y3 * y3 - y1 * y1 + x3 * x3 - x1 * x1 + d1 * d1 - d3 * d3) /
-				(2 * (x3 - x1))
-			let delta_beta = beta1 - beta2
-			let delta_alpha = alpha2 - alpha1
-			let X = alpha1 * (delta_beta / delta_alpha) + beta1
-			let Y = delta_beta / delta_alpha
-			console.log(alpha1, alpha2, beta1, beta2, delta_alpha, delta_beta)
-			console.log('X = ', X, 'Y = ', Y)
-			console.log('\n')
+
+			const [x1, y1, d1, alpha1] = extractFlagCoordsAndDistance(
+				observedFlags[0]
+			)
+			const [x2, y2, d2, alpha2] = extractFlagCoordsAndDistance(
+				observedFlags[1]
+			)
+			const [x3, y3, d3, alpha3] = extractFlagCoordsAndDistance(
+				observedFlags[2]
+			)
+			const [X, Y] = this.calculatePosition(x1, y1, d1, x2, y2, d2, x3, y3, d3)
+
+			console.log(
+				`${this.id} игрок команды ${this.team}: X = ${round(X)} Y = ${round(Y)}`
+			)
+
+			// Opponent
+			if (opponents.length) {
+				// console.log('opponents', opponents)
+				const [da, alphaa] = [opponents[0].p[0], opponents[0].p[1]]
+				const da1 = Math.sqrt(
+					d1 * d1 +
+						da * da -
+						2 * d1 * da * Math.cos(Math.abs(alpha1 - alphaa) * (Math.PI / 180))
+				)
+				const da2 = Math.sqrt(
+					d2 * d2 +
+						da * da -
+						2 * d2 * da * Math.cos(Math.abs(alpha2 - alphaa) * (Math.PI / 180))
+				)
+				const [XO, YO] = this.calculatePosition(
+					x1,
+					y1,
+					da1,
+					x2,
+					y2,
+					da2,
+					X,
+					Y,
+					da
+				)
+				console.log(
+					`${this.id} игрок команды ${this.team} видит игрока команды ${
+						opponents[0].cmd.p[1]
+					}: X = ${round(XO)} Y = ${round(YO)}`
+				)
+			}
 		}
+	}
+	calculatePosition(x1, y1, d1, x2, y2, d2, x3, y3, d3) {
+		const alpha1 = (y1 - y2) / (x2 - x1)
+		const beta1 =
+			(y2 * y2 - y1 * y1 + x2 * x2 - x1 * x1 + d1 * d1 - d2 * d2) /
+			(2 * (x2 - x1))
+		const alpha2 = (y1 - y3) / (x3 - x1)
+		const beta2 =
+			(y3 * y3 - y1 * y1 + x3 * x3 - x1 * x1 + d1 * d1 - d3 * d3) /
+			(2 * (x3 - x1))
+		const delta_beta = beta1 - beta2
+		const delta_alpha = alpha2 - alpha1
+		const X = alpha1 * (delta_beta / delta_alpha) + beta1
+		const Y = delta_beta / delta_alpha
+		return [X, Y]
 	}
 	sendCmd() {
 		if (this.run) {
--- app.js
+++ app.js
@@ -1,17 +1,45 @@
+const readline = require('readline')
 const Agent = require('./agent')
 const Socket = require('./socket')
 const VERSION = 7
+const INPUT = true
 
-let teamNameA = 'teamA'
-let teamNameB = 'teamB'
+const teamNameA = 'A'
+const teamNameB = 'B'
 
-let pA1 = new Agent()
-let pB1 = new Agent()
+async function getUserInput(prompt) {
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout,
+	})
+	const it = rl[Symbol.asyncIterator]()
+
+	console.log(prompt)
+	const input = (await it.next()).value.split(' ').map(a => +a)
+
+	rl.close()
+	return input
+}
 
 ;(async () => {
-	await Socket(pA1, teamNameA, VERSION)
-	// await Socket(pB1, teamNameB, VERSION);
+	let c1, c2, s
 
-	await pA1.socketSend('move', `-15 0`)
-	// await pB1.socketSend('move', `-5 10`);
+	if (INPUT) {
+		c1 = await getUserInput('First player coordinates (x y):')
+		c2 = await getUserInput('Second player coordinates (x y):')
+		s = +(await getUserInput('First player rotation speed (s):'))
+	} else {
+		;[c1, c2, s] = [[-15, 0], [5, 10], 20]
+	}
+
+	console.log('kekaaaaa', [c1, c2, s])
+
+	let pA1 = new Agent(teamNameA, { name: 'spin', speed: s })
+	let pB1 = new Agent(teamNameB)
+
+	await Socket(pA1, pA1.team, VERSION)
+	await Socket(pB1, pB1.team, VERSION)
+
+	await pA1.socketSend('move', `${c1[0]} ${c1[1]}`)
+	await pB1.socketSend('move', `${-c2[0]} ${-c2[1]}`)
 })()
